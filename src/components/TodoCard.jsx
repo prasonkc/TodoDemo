5303a3bc0431525b64096e3b7cdc4ecdef51cb1d
Prason KC <prasonkc07@gmail.com>
2025-12-14 19:58:04 +0545

    Update todo cards

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 
-const TodoCard = ({todo}) => {
+const TodoCard = ({ todo }) => {
   return (
     <div class="w-[70%] mx-auto rounded-xl bg-gray-600 backdrop-blur-lg border border-black shadow-xl flex justify-between p-5 m-5">
       <div className="contents flex flex-col">
-        <h3 class="text-xl font-semibold text-black">
-          {todo.title}
-        </h3>
+        <div className="flex gap-10 items-center">
+          <h3 class="text-xl font-semibold text-black">{todo.title}</h3>
 
-        <p class="mt-2 text-sm text-black">
-          {todo.description}
-        </p>
+          <span
+            className={`text-sm ${
+              todo.status === "completed" ? "text-green-600" : "text-yellow-600"
+            }`}
+          >
+            {todo.status}
+          </span>
+        </div>
+
+        <p class="mt-2 text-sm text-black">{todo.description}</p>
+
+        {todo.image && (
+          <img src={todo.image} className="w-16 h-16 object-cover" />
+        )}
       </div>
 
       <div className="buttons flex gap-5">
---
+++ src/pages/createTodos.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const createTodos = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default createTodos
